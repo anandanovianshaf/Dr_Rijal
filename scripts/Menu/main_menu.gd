60f16dad81b3b6c47c162f2dae4a3b22dd30db0d
Dimas Rizqia Hidayat <dimaswae012@gmail.com>
2025-11-12 23:53:38 +0700

    Check Point

--- scripts/Menu/main_menu.gd
+++ scripts/Menu/main_menu.gd
@@ -1,67 +1,40 @@
 extends Control
 
-@onready var start_button: TextureButton = $VBoxContainer/StartButton
-@onready var credit_button: TextureButton = $VBoxContainer/CreditButton
-@onready var quit_button: TextureButton = $VBoxContainer/QuitButton
-@onready var title_label: Label = $Title
-@onready var fade_rect: ColorRect = $FadeRect
-@onready var anim: AnimationPlayer = $AnimationPlayer
-@onready var sfx_hover: AudioStreamPlayer = $SFX_Hover
-@onready var sfx_click: AudioStreamPlayer = $SFX_Click
+@onready var play_button: TextureButton = $MenuContainer/PlayButton
+@onready var tutorial_button: TextureButton = $MenuContainer/TutorialButton
+@onready var credit_button: TextureButton = $MenuContainer/CreditButton
+@onready var quit_button: TextureButton = $MenuContainer/QuitButton
+
+@onready var sfx_hover: AudioStreamPlayer = $AudioHover
+@onready var sfx_click: AudioStreamPlayer = $AudioClick
 
 func _ready():
-	# Awal kondisi fade
-	fade_rect.modulate.a = 1.0
-	anim.play("fade_in")
+	# kumpulkan semua tombol biar gak connect satu-satu manual
+	var buttons = [
+		play_button,
+		tutorial_button,
+		credit_button,
+		quit_button
+	]
 
-	# Connect tombol
-	start_button.pressed.connect(_on_start_pressed)
-	credit_button.pressed.connect(_on_credit_pressed)
-	quit_button.pressed.connect(_on_quit_pressed)
+	for btn in buttons:
+		btn.mouse_entered.connect(_on_button_hovered)
+		btn.pressed.connect(_on_button_pressed.bind(btn))
 
-	# Hover efek suara dan skala
-	for btn in [start_button, credit_button, quit_button]:
-		btn.mouse_entered.connect(func(): _on_button_hovered(btn))
-		btn.mouse_exited.connect(func(): _on_button_exited(btn))
-
-	# Intro animasi menu
-	title_label.modulate.a = 0
-	for btn in [start_button, credit_button, quit_button]:
-		btn.modulate.a = 0
-	anim.play("menu_intro")
-
-
-func _on_start_pressed():
-	_play_click_sfx()
-	anim.play("fade_out")
-	await anim.animation_finished
-	get_tree().change_scene_to_file("res://scenes/Menu/prologue.tscn")
-
-
-func _on_credit_pressed():
-	_play_click_sfx()
-	anim.play("fade_out")
-	await anim.animation_finished
-	get_tree().change_scene_to_file("res://scenes/Menu/credits.tscn")
-
-
-func _on_quit_pressed():
-	_play_click_sfx()
-	get_tree().quit()
-
-
-func _on_button_hovered(btn: TextureButton):
+func _on_button_hovered():
 	if sfx_hover:
 		sfx_hover.play()
-	var tween = create_tween()
-	tween.tween_property(btn, "scale", Vector2(1.1, 1.1), 0.15).set_trans(Tween.TRANS_SINE)
 
-
-func _on_button_exited(btn: TextureButton):
-	var tween = create_tween()
-	tween.tween_property(btn, "scale", Vector2(1.0, 1.0), 0.15).set_trans(Tween.TRANS_SINE)
-
-
-func _play_click_sfx():
+func _on_button_pressed(button: TextureButton):
 	if sfx_click:
 		sfx_click.play()
+	
+	match button.name:
+		"PlayButton":
+			get_tree().change_scene_to_file("res://scenes/Prologue.tscn")
+		"TutorialButton":
+			get_tree().change_scene_to_file("res://scenes/Tutorial.tscn")
+		"CreditButton":
+			get_tree().change_scene_to_file("res://scenes/MainMenu/credits.tscn")
+		"QuitButton":
+			get_tree().quit()
